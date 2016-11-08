2dc5fa13a53e98d85cc8aaa500f006a9b585d267
Akash Tolangee <atolangee@gmail.com>
2016-11-07 18:44:33 -0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,14 +6,10 @@
 		<p> Copyright  <?php print (" " . date ('Y') . " "); ?>. | Akash Tolangee. | All rights Reserved. | Designed by Akash.</p>
 </footer>
 
-</div><!-- END WRAPPER -->
+</div>
 
-
-<?php /* wp_footer function must be last item before closing body tag: many plugins use this hook to reference JavaScript files */ ?>
 <?php wp_footer(); ?> 
-<!-- begin validation buttons -->
 
-<!-- end validation buttons --> 
 </body>
 
 </html>
