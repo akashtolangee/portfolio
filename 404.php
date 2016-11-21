9ed3f44976b200bfb21a5b2de6976a4c31c24dbf
Akash Tolangee <atolangee@gmail.com>
2016-11-21 15:42:10 -0800

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<?php get_sidebar('primary'); ?>
+
+	
+<div id= "main">		
+<div id="content">
+
+        <h2 class="page-title"><?php _e( 'Not Found', ' NAHH 2016' ); ?></h2>
+			
+
+<p><?php _e( 'It looks the page you have been looking for could not be found. Maybe the page has been moved. Please us the search.', 'NAHH' ); ?></p>
+
+					<?php get_search_form(); ?>
+				</div>
+			
+
+		</div>
+	
+<?php get_sidebar('secondary'); ?> 
+
+<?php get_footer(); ?>
