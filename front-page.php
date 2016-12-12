520d480d6accd3e016108f40f5d37190ddc1f26a
Akash Tolangee <atolangee@gmail.com>
2016-12-11 21:41:58 -0800

    Add files via upload
    Assignment 10

---
+++ front-page.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<?php if(function_exists("get_smooth_slider")){get_smooth_slider($slider_id="1");}?>
+
+<?php get_sidebar('primary'); ?>
+
+<div id="main">
+<div id="content" class="index">
+            
+<?php if (have_posts()) : ?>
+
+    <?php while (have_posts()) : the_post(); ?>
+    	
+    	<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+     
+		<?php the_content('More &raquo;'); ?>
+     	<?php edit_post_link('Edit this entry.', '<p><small>', '</small></p>'); ?>
+        </div>
+</div>
+ <?php endwhile; endif; ?>
+ 
+
+<?php get_footer(); ?>
